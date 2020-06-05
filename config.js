4b39314d62a7ae6cc19d592b59c1e9041e6de439
Andrew Kalek <andrew.kalek@anlek.com>
2020-06-05 12:08:28 -0700

    :truck: config to a JS file
    Also tweaked users object

---
+++ config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  users: {
+    steve: {password: "password", regionId: "1"}, // Whitehorse
+    frank: {password: "password", regionId: "9"}, // Mayo
+    jack: {password: "password", regionId: "15"}, // Watson Lake
+  }
+  ,
+  regions: {
+    1: { label: "Whitehorse", value: "1" },
+    2: { label: "Beaver Creek", value: "2" },
+    3: { label: "Burwash Landing", value: "3" },
+    4: { label: "Carcross and Tagish", value: "4" },
+    5: { label: "Carmacks", value: "5" },
+    6: { label: "Dawson City", value: "6" },
+    7: { label: "Faro", value: "7" },
+    8: { label: "Haines Junction", value: "8" },
+    9: { label: "Mayo", value: "9" },
+    10: { label: "Mount Lorne", value: "10" },
+    11: { label: "Old Crow", value: "11" },
+    12: { label: "Pelly Crossing", value: "12" },
+    13: { label: "Ross River", value: "13" },
+    14: { label: "Teslin", value: "14" },
+    15: { label: "Watson Lake", value: "15" },
+    16: { label: "Outside Yukon", value: "16" }
+  }
+}
